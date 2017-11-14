c71b32657048c58876bcc0c29bd366309605ce57
keidrun <kssk96@gmail.com>
2017-11-14 15:47:40 +0900

    Add base.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# boilerplate-gulp-postcss
-Boilerplate of PostCSS with Gulp.
+# boilerplate-gulp-postcss-cssnext [![devDependency Status][depstat-image]][depstat-url]
+Boilerplate of PostCSS & CSSNext with Gulp.
+
+## Description
+This boilerplate provides an automatic web development environment with gulp to develop HTML, CSS(PostCSS&CSSNext) and Javascript(ES6/ES2015).
+
+## Steps
+
+1. Clone this repository via `git`.
+1. Install modules via `yarn install` or `npm install`.
+1. Execute `gulp` and See `http://localhost:3000/` on your Chrome.
+1. Enjoy developing your web site.
+
+## Note
+
+- You must reload your browser after `gulp` or `gulp watch` because of [the BrowserSync issue](https://github.com/BrowserSync/browser-sync/issues/1015).
+
+[depstat-url]: https://david-dm.org/keidrun/boilerplate-gulp-postcss-cssnext?type=dev
+[depstat-image]: https://david-dm.org/keidrun/boilerplate-gulp-postcss-cssnext/dev-status.svg
---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+const gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  browserSync = require('browser-sync').create(),
+  concat = require('gulp-concat'),
+  plumber = require('gulp-plumber'),
+  sourcemaps = require('gulp-sourcemaps'),
+  rename = require('gulp-rename'),
+  del = require('del'),
+  zip = require('gulp-zip'),
+  sequence = require('run-sequence'),
+  babel = require('gulp-babel'),
+  imagemin = require('gulp-imagemin'),
+  imageminPngquant = require('imagemin-pngquant'),
+  imageminJpegRecompress = require('imagemin-jpeg-recompress');
+
+const postcss = require('gulp-postcss'),
+  cssnext = require('postcss-cssnext');
+
+const DIST_PATH = 'public/dist',
+  IMAGES_PATH = 'public/images/**/*.{png,jpeg,jpg,svg,gif}',
+  SCRIPTS_PATH = 'public/scripts/**/*.js',
+  STYLES_PATH = 'public/styles/**/*.css',
+  HTML_PATH = 'public/*.html',
+  EXPORT_PATH = 'export',
+  BROWSERS = ['last 2 versions', 'ie 8'];
+
+gulp.task('clean', () => {
+  return del.sync([DIST_PATH, EXPORT_PATH]);
+});
+
+gulp.task('images', () => {
+  return gulp
+    .src(IMAGES_PATH)
+    .pipe(
+      imagemin([
+        imagemin.gifsicle(),
+        imagemin.jpegtran(),
+        imagemin.optipng(),
+        imagemin.svgo(),
+        imageminPngquant(),
+        imageminJpegRecompress()
+      ])
+    )
+    .pipe(gulp.dest(DIST_PATH + '/images'));
+});
+
+gulp.task('styles', () => {
+  const plugins = [
+    cssnext({
+      browsers: BROWSERS,
+      // TODO: Waiting for update because options not working.
+      compress: false,
+      import: false,
+      warnForDuplicates: false
+    }),
+    // TODO: Delete if cssnext's options work or add more features.
+    require('postcss-import'),
+    require('postcss-url'),
+    require('postcss-browser-reporter'),
+    require('postcss-reporter'),
+    require('postcss-custom-properties'),
+    require('postcss-custom-media'),
+    require('postcss-nested'),
+    require('postcss-mixins'),
+    require('postcss-color-rgba-fallback'),
+    require('pixrem'),
+    require('postcss-size'),
+    require('postcss-selector-matches'),
+    require('postcss-normalize')({ browsers: BROWSERS, forceImport: true }),
+    require('cssnano')
+  ];
+  return gulp
+    .src('public/styles/styles.css')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DIST_PATH))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', () => {
+  return gulp
+    .src(SCRIPTS_PATH)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['es2015'],
+        plugins: ['transform-es2015-modules-umd']
+      })
+    )
+    .pipe(uglify())
+    .pipe(concat('scripts.js'))
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DIST_PATH))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', () => {
+  return gulp.src(HTML_PATH).pipe(gulp.dest(DIST_PATH));
+});
+
+gulp.task('build', () => {
+  return sequence('clean', ['images', 'styles', 'scripts', 'html']);
+});
+
+gulp.task('export', () => {
+  return gulp
+    .src(DIST_PATH + '/**/*')
+    .pipe(zip('website.zip'))
+    .pipe(gulp.dest(EXPORT_PATH));
+});
+
+gulp.task('watch', ['build'], () => {
+  browserSync.init({
+    server: DIST_PATH,
+    index: 'index.html'
+  });
+  gulp.watch(STYLES_PATH, ['styles']);
+  gulp.watch(SCRIPTS_PATH, ['scripts']);
+  gulp.watch(HTML_PATH, ['html']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch'], () => {});
